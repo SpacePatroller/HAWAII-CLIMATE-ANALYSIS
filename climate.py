2881b51f50c9e9741713d0be12b7b2d53e3ef48e
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2018-12-06 22:33:03 -0600

    last part almost done

--- climate.py
+++ climate.py
@@ -2,7 +2,7 @@ import numpy as np
 
 import pandas as pd
 import datetime as dt
-
+from datetime import datetime
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -31,9 +31,10 @@ def home():
     
     return(
         f"Lets hope this information sticks!</br>"
-        f"/api/v1.0/precipitation</br>"
-        f"Station List: /api/v1.0/stations</br>"
-        f"12 Month Temps: /api/v1.0/tobs"
+        f"Precip:-----------------/api/v1.0/precipitation</br>"
+        f"Station List:----------/api/v1.0/stations</br>"
+        f"12 Month Temps:---/api/v1.0/tobs</br>"
+        f"Min Max Avg From:--/api/v1.0/yyyy dd mm"
     )
 
 # Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
@@ -80,6 +81,39 @@ def tobs():
         filter(Measurement.date > date_twelve).all()
 
     return jsonify(max_station_12)
+
+
+#   `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
+
+#   * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+
+
+
+#   * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
+
+
+
+#   * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
+
+@app.route("/api/v1.0/<date>")
+def start_date(date):
+
+    date = datetime.strptime(date, '%Y%m%d')
+    date_ = date.date()
+    
+    min_max_avg_temp = session.query((Measurement.date),func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
+    filter(Measurement.date >= date_).group_by(Measurement.date).all()
+    
+    data = list(np.ravel(min_max_avg_temp))
+    
+    return jsonify(data)
+
+    
+    #push all three to a dictionary and then return the dictionary?
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
