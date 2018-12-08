d027b31f11d3b9126b17c114b6dbcae6dbb17baf
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2018-12-08 17:35:25 -0600

    HW

--- climate.py
+++ climate.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask , jsonify
+from flask import Flask, jsonify
 
 
 engine = create_engine("sqlite:///hawaii.sqlite")
@@ -25,10 +25,11 @@ session = Session(engine)
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     print("Server received request for 'Home' page...")
-    
+
     return(
         f"Most Advanced Technologicaly ahead Climate App!</br>"
         f"<a href=test></a>"
@@ -42,15 +43,16 @@ def home():
 # Convert the query results to a Dictionary using `date` as the key and `prcp` as the value.
 # Return the JSON representation of your dictionary.
 
+
 @app.route("/api/v1.0/precipitation")
 def prcp():
 
     # All info from Measurment Table
-    results = session.query(Measurement.date,Measurement.prcp).all()
+    results = session.query(Measurement.date, Measurement.prcp).all()
 
     weather_data = []
     for weather in results:
-        weather_dict = {}    
+        weather_dict = {}
         weather_dict[weather.date] = weather.prcp
         weather_data.append(weather_dict)
 
@@ -68,18 +70,19 @@ def station():
 # query for the dates and temperature observations from a year from the last data point.
 # Return a JSON list of Temperature Observations (tobs) for the previous year.
 
+
 @app.route("/api/v1.0/tobs")
 def tobs():
 
-    #find last data point
+    # find last data point
     #last_date = session.query(func.max(Measurement.date)).all()
     conn = engine.connect()
-    measurements = pd.read_sql("Select * From Measurement" ,conn)
+    measurements = pd.read_sql("Select * From Measurement", conn)
     measurements['date'] = pd.to_datetime(measurements['date'])
     twelve_months_ago = measurements['date'].max() - pd.DateOffset(months=12)
     date_twelve = twelve_months_ago.date()
 
-    max_station_12 = session.query(Measurement.date,Measurement.tobs).\
+    max_station_12 = session.query(Measurement.date, Measurement.tobs).\
         filter(Measurement.date > date_twelve).all()
 
     return jsonify(max_station_12)
@@ -91,20 +94,20 @@ def tobs():
 #   * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
 
 @app.route("/api/v1.0/<start>/<end>")
-def start_date(start,end):
+def start_date(start, end):
 
     start = datetime.strptime(start, '%Y%m%d')
     start_date = start.date()
 
     end = datetime.strptime(end, '%Y%m%d')
     end_date = end.date()
-    
-    min_max_avg_temp = session.query((Measurement.date),func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
-    filter(Measurement.date >= start_date).group_by(Measurement.date).\
-    filter(Measurement.date <= end_date).all()
-    
+
+    min_max_avg_temp = session.query((Measurement.date), func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).group_by(Measurement.date).\
+        filter(Measurement.date <= end_date).all()
+
     data = list(np.ravel(min_max_avg_temp))
-    
+
     return jsonify(data)
 
 
@@ -115,19 +118,15 @@ def start(date):
 
     date = datetime.strptime(date, '%Y%m%d')
     date_ = date.date()
-    
-    min_max_avg_temp = session.query((Measurement.date),func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
-    filter(Measurement.date >= date_).group_by(Measurement.date).all()
-    
+
+    min_max_avg_temp = session.query((Measurement.date), func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= date_).group_by(Measurement.date).all()
+
     data = list(np.ravel(min_max_avg_temp))
-    
+
     return jsonify(data)
 
-    
-    #push all three to a dictionary and then return the dictionary?
-
-
-
+    # push all three to a dictionary and then return the dictionary?
 
 
 if __name__ == "__main__":
